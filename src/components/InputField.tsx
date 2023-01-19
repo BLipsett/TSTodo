f652665cbe318991952d7feb13574e8f15732397
Brian Lipsett <lipsettbrian3@gmail.com>
2023-01-18 18:55:36 -0700

    setTodos

---
+++ src/components/InputField.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+interface Props  {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+
+const InputField = ({todo, setTodo, handleAdd}: Props) => {
+  return (
+    <form className="input" onSubmit={handleAdd}
+        > 
+      <input type="input" 
+      placeholder='Enter task title' 
+      className='input_field'
+      value={todo}
+      onChange={ (e) => {
+        setTodo(e.target.value)
+      }}
+      />
+      <button 
+        type="submit" 
+        className='input_button'
+        >Enter</button>
+    </form>
+  )
+}
+
+export default InputField
+
